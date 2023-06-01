12cca28127115e9bc12f0cf93296339d1e2bc4a8
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-01 10:29:12 +0530

    added new timer

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import queryClient from "queryClient";
 import { Store } from "store/Store";
 import { NotificationsProvider } from "reapop";
 import { Notification } from "components/notification";
+import { TimerProvider } from "store/Timer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -16,7 +17,9 @@ root.render(
       <NotificationsProvider>
         <Notification />
         <Store>
-          <App />
+          <TimerProvider>
+            <App />
+          </TimerProvider>
         </Store>
       </NotificationsProvider>
     </QueryClientProvider>
